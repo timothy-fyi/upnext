c599483a3c33a313a2ec390d1c61af041643703b
Tim <107897528+timothy-fyi@users.noreply.github.com>
2024-02-07 13:51:43 -0500

    Update upnext.py
    updated to adjust to YouTubes code changes

--- upnext.py
+++ upnext.py
@@ -17,46 +17,52 @@ import time
 gc_driver = 'CHROME_DRIVER_LOCATION'
 gc_profile = 'CHROMIUM_PROFILE_LOCATION'
 playlist_link = 'PLAYLIST_LINK'
-playlist_name = 'EXACT PLAYLIST NAME HERE'
+playlist_name = 'EXACT PLAYLSIT NAME HERE'
 subs_to_keep = ['CHANNEL', 'NAMES', 'AS', 'LIST']
 
 # browser settings
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=" + gc_profile)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_experimental_option("excludeSwitches", ['enable-automation'])
-options.add_experimental_option('useAutomationExtension', False)
 ser = Service(gc_driver)
 browser = webdriver.Chrome(service=ser, options=options)
 
+# access playlist
 browser.get(playlist_link)
+
 time.sleep(7)
-# have to fix below line, supposed to automitcally return playlist name without user input from 'user settings', broke after a recent Youtube update
-# playlist_name = browser.find_element(By.XPATH, '//*[@id="text-displayed"]').text
+# get playlist name for later reference when adding videos
+# have to fix below lines, supposed to automitcally return playlist name without user input
+# playlist_name_element = browser.find_element(By.XPATH, '//*[@id="text"]')
 
 videos_to_remove = browser.find_elements(By.XPATH, '//*[@id="video-title"]')
 for videos in videos_to_remove:
-    hover = ActionChains(browser).move_to_element(videos)
-    hover.perform()
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(3) > ytd-menu-renderer:nth-child(1)'))).click()
-    time.sleep(1)
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-service-item-renderer.style-scope:nth-child(4) > tp-yt-paper-item:nth-child(1)'))).click()
-    time.sleep(1)
+    try:
+        hover = ActionChains(browser).move_to_element(videos)
+        hover.perform()
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(3) > ytd-menu-renderer:nth-child(1)'))).click()
+        time.sleep(1)
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-service-item-renderer.style-scope:nth-child(4) > tp-yt-paper-item:nth-child(1)'))).click()
+        time.sleep(1)
+    except ElementNotInteractableException:
+        pass
 
+# access subscription page, and add new videos to playlist
 time.sleep(3)
-browser.get("https://www.youtube.com/feed/subscriptions")
+browser.get('https://www.youtube.com/feed/subscriptions')
 
+# identifys video elements
 time.sleep(3)
-video_elements = browser.find_elements(By.CLASS_NAME, 'style-scope ytd-grid-video-renderer')
+video_elements = browser.find_elements(By.CLASS_NAME, 'style-scope ytd-rich-grid-media')
 
 time.sleep(3)
 subs_to_keep = [sub.lower() for sub in subs_to_keep]
 for element in video_elements:
     channel_names = element.find_elements(By.CLASS_NAME, 'style-scope ytd-channel-name')
-    for name in channel_names:
+    for name in channel_names:    
         if name.text.lower() in subs_to_keep:
             # determines if video has been watched yet
-            try:
+            try:                      
                 watched_flag = element.find_element(By.CLASS_NAME, 'style-scope ytd-thumbnail-overlay-resume-playback-renderer')
             except NoSuchElementException:
                 hover_two = ActionChains(browser).move_to_element(name)
@@ -66,16 +72,16 @@ for element in video_elements:
                 time.sleep(2)
                 # add to playlist click
                 WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-service-item-renderer.style-scope:nth-child(3) > tp-yt-paper-item:nth-child(1)'))).click()
-                time.sleep(1)
+                time.sleep(1)                
                 # identify correct playlist
-                playlist_name_path = '//*[contains(text(), "' + playlist_name + '")]'
+                playlist_name_path = '//*[contains(text(), "'+ playlist_name +'")]'
                 # adds all elements that have playlist name text to list
-                playlist_list = browser.find_elements(By.XPATH, playlist_name_path)
+                correct_playlist_check = browser.find_elements(By.XPATH, playlist_name_path)
                 # try to click each item in list, if error, move to the next
-                for playlist in playlist_list:
+                for check in correct_playlist_check:
                     time.sleep(1)
                     try:
-                        playlist.click()
+                        check.click()
                     except (ElementClickInterceptedException, ElementNotInteractableException) as error:
                         pass
                 time.sleep(4)
