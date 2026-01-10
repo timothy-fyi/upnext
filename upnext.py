031c5d233fc00f62a1045a655ab1c5ca8a300a24
Tim <107897528+timothy-fyi@users.noreply.github.com>
2026-01-10 16:58:57 -0500

    Load settings from YAML and improve video handling
    Refactor upnext.py to load user settings from a YAML file and update subscription handling logic.

--- upnext.py
+++ upnext.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -12,15 +13,20 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import json
+import yaml
+
+with open('/home/pi/upnext_settings.yaml', 'r') as upnext_settings:
+    settings = yaml.safe_load(upnext_settings)
 
 # user settings
-gc_driver = 'CHROME_DRIVER_LOCATION'
-gc_profile = 'CHROMIUM_PROFILE_LOCATION'
-playlist_link = 'PLAYLIST_LINK'
-playlist_name = 'EXACT PLAYLIST NAME HERE'
-subs_to_keep = ['CHANNEL', 'NAMES', 'AS', 'LIST']
+gc_driver = settings['gc_driver']
+gc_profile = settings['gc_profile']
+playlist_link = settings['playlist_link']
+playlist_name = settings['playlist_name']
+subs_to_keep = settings['subs']
 
-# browser settings
+# browswer settings
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=" + gc_profile)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -29,11 +35,12 @@ browser = webdriver.Chrome(service=ser, options=options)
 
 # access playlist
 browser.get(playlist_link)
-
 time.sleep(7)
+
 # get playlist name for later reference when adding videos
-# have to fix below lines, supposed to automatically return playlist name without user input
+# have to fix below lines, supposed to automitcally return playlist name without user input
 # playlist_name_element = browser.find_element(By.XPATH, '//*[@id="text"]')
+# playlist_name = playlist_name_element.text
 
 videos_to_remove = browser.find_elements(By.XPATH, '//*[@id="video-title"]')
 for videos in videos_to_remove:
@@ -51,27 +58,28 @@ for videos in videos_to_remove:
 time.sleep(3)
 browser.get('https://www.youtube.com/feed/subscriptions')
 
-# identify video elements
-time.sleep(3)
-video_elements = browser.find_elements(By.CLASS_NAME, 'style-scope ytd-rich-grid-media')
+# identifys video elements
+time.sleep(5)
+video_elements = browser.find_elements(By.CSS_SELECTOR, 'ytd-rich-item-renderer, ytd-grid-video-renderer, ytd-video-renderer')
 
-time.sleep(3)
-subs_to_keep = [sub.lower() for sub in subs_to_keep]
+# adds selected channel subscriptions to playlist
+time.sleep(5)
 for element in video_elements:
-    channel_names = element.find_elements(By.CLASS_NAME, 'style-scope ytd-channel-name')
+    channel_names = element.find_elements(By.CSS_SELECTOR, 'a.yt-core-attributed-string__link')
+
     for name in channel_names:    
-        if name.text.lower() in subs_to_keep:
-            # determines if video has been watched yet
-            try:                      
-                watched_flag = element.find_element(By.CLASS_NAME, 'style-scope ytd-thumbnail-overlay-resume-playback-renderer')
+        if name.text in subs_to_keep:
+            try:        
+                watched_flag = element.find_element(By.CLASS_NAME, 'ytThumbnailOverlayProgressBarHostWatchedProgressBarSegment')
+            # adds videos that haven't been watched to playlist
             except NoSuchElementException:
                 hover_two = ActionChains(browser).move_to_element(name)
                 hover_two.perform()
                 # finds video playlist options
-                WebDriverWait(element, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'style-scope ytd-menu-renderer'))).click()
+                WebDriverWait(element, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-renderer yt-icon-button, ytd-menu-renderer button, .yt-lockup-metadata-view-model__menu-button button'))).click()
                 time.sleep(2)
-                # add to playlist click
-                WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-service-item-renderer.style-scope:nth-child(3) > tp-yt-paper-item:nth-child(1)'))).click()
+                # add to playlist click 
+                WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'yt-list-item-view-model.yt-list-item-view-model:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(1)'))).click()
                 time.sleep(1)                
                 # identify correct playlist
                 playlist_name_path = '//*[contains(text(), "'+ playlist_name +'")]'
@@ -85,9 +93,10 @@ for element in video_elements:
                     except (ElementClickInterceptedException, ElementNotInteractableException) as error:
                         pass
                 time.sleep(4)
-                # close out of menu options
-                WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'yt-icon.ytd-add-to-playlist-renderer'))).click()
-                time.sleep(4)
+                # close out of menu options -> possibly obsolete with recent Youtube update
+                # WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'yt-icon.ytd-add-to-playlist-renderer'))).click()
+                # time.sleep(4)
 
+# quits
 time.sleep(3)
 browser.quit()
