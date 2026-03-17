ea75e70691c2d946d40ea73d153c5ab097d9dad5
Tim <tjflanagan27@gmail.com>
2026-03-16 21:20:46 -0400

    complete refactor. also added requirements.txt

--- README.md
+++ README.md
@@ -11,19 +11,21 @@
 - Chromium (Linux)
 
 # Basic Setup and Instructions
-1) Upnext now automatically detects and downloads the appropriate driver for your chosen browser via the Selenium package. The exception is Chromium. In order to set up Upnext with Chromium, you need to download the correct ChromeDriver for your version of Chromium (if no version exists on your device already[^1]). It can be downloaded [here](https://chromedriver.chromium.org/).
-2) For supported browsers (like FireFox), it is recommended you create a new profile, specifically for use of this script. This will allow you to use the same browser at the same time the script is running. If you attempt to run Upnext from the same profile you currently have a browser window up in, the script will not run correctly.
-3) The script **does not** require your login information. Open your browser, switch to the Profile you created for Upnext (if you did), log into your YouTube account, and ensure your login details are saved. Close the browser, reopen it, and go to YouTube to ensure you are still logged in. Once confirmed, close your browser once again.
-4) Create a new playlist in your YouTube account. You can use an existing one, but it is recommeneded to keep your automation playlist separate. 
-5) Run upnext.py once to generate your "upnext_settings.yaml" file. Once done, open it up and update your settings:
+1) Install dependencies: ```pip install -r requirements.txt```
+2) Upnext now automatically detects and downloads the appropriate driver for your chosen browser via the Selenium package. The exception is Chromium. In order to set up Upnext with Chromium, you need to download the correct ChromeDriver for your version of Chromium (if no version exists on your device already[^1]). It can be downloaded [here](https://chromedriver.chromium.org/).
+3) For supported browsers (like FireFox), it is recommended you create a new profile, specifically for use of this script. This will allow you to use the same browser at the same time the script is running. If you attempt to run Upnext from the same profile you currently have a browser window up in, the script will not run correctly.
+4) The script **does not** require your login information. Open your browser, switch to the Profile you created for Upnext (if you did), log into your YouTube account, and ensure your login details are saved. Close the browser, reopen it, and go to YouTube to ensure you are still logged in. Once confirmed, close your browser once again.
+5) Create a new playlist in your YouTube account. You can use an existing one, but it is recommeneded to keep your automation playlist separate. 
+6) Run upnext.py once to generate your "upnext_settings.yaml" file. Once done, open it up and update your settings:
 
    * **browser_choice** - Your chosen browser, contingent upon compatibility (see above). Case insensitive.
    * **browswer_profile** - This is the location of your browser user profile. It is located in different locations depending on the 
    browser. For example, for Chromium it may be located at ```/home/your_name/.config/chromium/```, but FireFox it may be located at ```C:\Users\yourname\AppData\Roaming\Mozilla\Firefox\Profiles\RandomString.Profile Number```
-   * **edge_profile** - Required if using Edge browser. The name of your profile (i.e. Profile 1, Profile 2, etc)
+   * **edge_profile** - Required if using Edge browser. The name of your profile (i.e. Profile 1, Profile 2, etc).
    * **chromium_driver** - The full location where you have your ChromeDriver saved. For example, ```/usr/lib/chromium-browser/chromedriver```
-   *  **playlist_link** - The direct link to your playlist on YouTube. It should look something like this: ```https://www.youtube.com/playlist?list=somerandomstringoflettersandnumbers```
-   *  **subs** - The list of channels you would like the script to add to your playlist. **Not** case sensitive. Ensure that the channels are separated by a new line with a dash. For example: 
+   * **playlist_link** - The direct link to your playlist on YouTube. It should look something like this: ```https://www.youtube.com/playlist?list=somerandomstringoflettersandnumbers```
+   * **playlist_name** - The name of your playlist. Case sensitive.
+   * **subs** - The list of channels you would like the script to add to your playlist. **Not** case sensitive. Ensure that the channels are separated by a new line with a dash. For example: 
    subs:
    - Channel 1
    - Channel 2
--- upnext.py
+++ upnext.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+import time
+import yaml
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -10,156 +14,204 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.common.action_chains import ActionChains
-import os
-import shutil
-import time
-import yaml
 
-def browser_setup(browser, profile_path):
-    if browser.lower() == 'firefox':
+
+def load_yaml(file, template=None):
+    if template:
+        if not os.path.exists(file):
+            try:
+                shutil.copy(template, file)
+                print(
+                    "Settings file missing. A new one has been created. You must open the new file and define the variables."
+                )
+            except FileNotFoundError:
+                print("Specified template file does not exist.")
+    try:
+        with open(file, "r") as config_file:
+            return yaml.safe_load(config_file)
+    except FileNotFoundError:
+        print("Settings file not found. Please check path.")
+
+
+def browser_setup(browser, profile_path, edge_profile_name=None, chromium_driver=None):
+    if browser.lower() == "firefox":
         options = FirefoxOptions()
         options.add_argument("-profile")
         options.add_argument(profile_path)
         return webdriver.Firefox(options=options)
-    
-    elif browser.lower() == 'chrome':
+
+    elif browser.lower() == "chrome":
         options = ChromeOptions()
-        options.add_argument('--disable-blink-features=AutomationControlled')
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        options.add_argument('--user-data-dir=' + profile_path)
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument("--user-data-dir=" + profile_path)
         return webdriver.Chrome(options=options)
-    
-    elif browser.lower() == 'edge':
+
+    elif browser.lower() == "edge":
         options = EdgeOptions()
-        options.add_argument('--user-data-dir=' + profile_path)
-        options.add_argument('--profile-directory=' + edge_profile_name)
+        options.add_argument("--user-data-dir=" + profile_path)
+        options.add_argument("--profile-directory=" + edge_profile_name)
         return webdriver.Edge(options=options)
-    
-    elif browser.lower() == 'chromium':
+
+    elif browser.lower() == "chromium":
         options = ChromeOptions()
-        options.add_argument('--disable-blink-features=AutomationControlled')
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        options.add_argument('--user-data-dir=' + profile_path)
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument("--user-data-dir=" + profile_path)
         service = Service(chromium_driver)
-        return webdriver.Chrome(service=service,options=options)
+        return webdriver.Chrome(service=service, options=options)
 
     else:
-        raise ValueError('Invalid browser specified. Current options are "firefox", "chrome", "edge", or "chromium".')
-    
-upnext_folder = os.path.dirname(__file__)
-settings_file = os.path.join(upnext_folder, 'upnext_settings.yaml')
-template_file = os.path.join(upnext_folder, 'settings_template.yaml')
-
-if template_file:
-    if not os.path.exists(settings_file):
-        try:
-            shutil.copy(template_file, settings_file)
-            print('upnext_settings.yaml was missing. It has now been created. You must open the new file and define the variables.')
-        except FileNotFoundError:
-            print('Specified template file does not exist.')
-        exit()
-
-try:
-    with open(settings_file, 'r') as upnext_settings:
-        settings = yaml.safe_load(upnext_settings)
-except FileNotFoundError:
-    print('upnext_settings.yaml not found.')
-    exit()
-
-browser_choice = settings['browser_choice']
-browser_profile = settings['browser_profile']
-edge_profile_name = settings['edge_profile_name']
-chromium_driver = settings['chromium_driver']
-playlist_link = settings['playlist_link']
-subs_to_keep = settings['subs']
-
-browser = browser_setup(browser=browser_choice, profile_path=browser_profile)
-
-# access playlist
-browser.get(playlist_link)
-time.sleep(7)
-
-# get playlist name for later reference when adding videos
-# the location can change depending on browser or even window size, so multiple selectors need to be used... currently
-# can possibly be improved by using a more general selector that looks for the playlist name text
-selectors = [
-    (By.CLASS_NAME, "yt-core-attributed-string--white-space-pre-wrap"),
-    (By.CLASS_NAME, "dynamicTextViewModelH1"),
-    (By.XPATH, "//div[@class='yt-page-header-view-model__scroll-container']//span[@role='text']"),
-    (By.XPATH, "//*[@id='page-header']/yt-page-header-renderer/yt-page-header-view-model/div/div[1]/div/yt-dynamic-text-view-model/h1/span"),   
-    (By.XPATH, "//*[@id='page-manager']/ytd-browse[1]/yt-page-header-renderer/yt-page-header-view-model/div[2]/div/div[1]/div/yt-dynamic-text-view-model/h1/span")
-]
-
-playlist_name = None
-for method, value in selectors:
-    try:
-        element = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located((method, value))
+        raise ValueError(
+            'Invalid browser specified. Current options are "firefox", "chrome", "edge", or "chromium".'
         )
-        if element.text:
-            playlist_name = element.text
-            break
-    except NoSuchElementException:
-        continue
 
-videos_to_remove = browser.find_elements(By.XPATH, '//*[@id="video-title"]')
-for videos in videos_to_remove:
-    try:
-        hover = ActionChains(browser).move_to_element(videos)
-        hover.perform()
-        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(3) > ytd-menu-renderer:nth-child(1)'))).click()
-        time.sleep(1)
-        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-service-item-renderer.style-scope:nth-child(4) > tp-yt-paper-item:nth-child(1)'))).click()
-        time.sleep(1)
-    except ElementNotInteractableException:
-        pass
 
-# access subscription page, and add new videos to playlist
-time.sleep(3)
-browser.get('https://www.youtube.com/feed/subscriptions')
+def playlist_cleanup(browser):
+    videos_to_remove = browser.find_elements(By.XPATH, '//*[@id="video-title"]')
+    for videos in videos_to_remove:
+        try:
+            hover = ActionChains(browser).move_to_element(videos)
+            hover.perform()
+            WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "ytd-playlist-video-renderer.style-scope:nth-child(1) > div:nth-child(3) > ytd-menu-renderer:nth-child(1)",
+                    )
+                )
+            ).click()
+            time.sleep(1)
+            WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "ytd-menu-service-item-renderer.style-scope:nth-child(4) > tp-yt-paper-item:nth-child(1)",
+                    )
+                )
+            ).click()
+            time.sleep(1)
+        except ElementNotInteractableException:
+            pass
 
-# identifys video elements
-time.sleep(5)
-video_elements = browser.find_elements(By.CSS_SELECTOR, 'ytd-rich-item-renderer')
 
-# adds selected channel subscriptions to playlist
-time.sleep(5)
-for element in video_elements:
-    browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
-    time.sleep(0.2)
+def playlist_build(browser, subs_to_keep, playlist_name):
+    video_elements = browser.find_elements(By.CSS_SELECTOR, "ytd-rich-item-renderer")
 
-    channel_names = element.find_elements(By.CSS_SELECTOR, "a.yt-core-attributed-string__link")
+    # adds selected channel subscriptions to playlist
+    time.sleep(5)
+    for element in video_elements:
+        browser.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'});", element
+        )
+        time.sleep(0.2)
 
-    for name in channel_names:    
-        if name.text in subs_to_keep:
-            # adds videos that haven't been watched to playlist - tries to find progress bar, if it does, skips, if not, adds to playlist
-            try:
-                watched_flag = element.find_element(By.CLASS_NAME, 'ytThumbnailOverlayProgressBarHostWatchedProgressBarSegment')
-                print(watched_flag.text)
-            except NoSuchElementException:
-                hover_two = ActionChains(browser).move_to_element(name)
-                hover_two.perform()
-                # finds video playlist options
-                WebDriverWait(element, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ytd-menu-renderer yt-icon-button, ytd-menu-renderer button, .yt-lockup-metadata-view-model__menu-button button'))).click()
-                time.sleep(2)
-                # add to playlist click 
-                WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Save to playlist")]'))).click()
-                time.sleep(1)                
-                # identify correct playlist
-                playlist_name_path = '//*[contains(text(), "'+ playlist_name +'")]'
-                # adds all elements that have playlist name text to list
-                correct_playlist_check = browser.find_elements(By.XPATH, playlist_name_path)
-                # try to click each item in list, if error, move to the next
-                for check in correct_playlist_check:
+        channel_names = element.find_elements(
+            By.CSS_SELECTOR, "a.yt-core-attributed-string__link"
+        )
+
+        for name in channel_names:
+            if name.text in subs_to_keep:
+                # adds videos that haven't been watched to playlist - tries to find progress bar, if it does, skips, if not, adds to playlist
+                try:
+                    watched_flag = element.find_element(
+                        By.CLASS_NAME,
+                        "ytThumbnailOverlayProgressBarHostWatchedProgressBarSegment",
+                    )
+                    print(watched_flag.text)
+                except NoSuchElementException:
+                    hover_two = ActionChains(browser).move_to_element(name)
+                    hover_two.perform()
+                    # finds video playlist options
+                    WebDriverWait(element, 20).until(
+                        EC.element_to_be_clickable(
+                            (
+                                By.CSS_SELECTOR,
+                                "ytd-menu-renderer yt-icon-button, ytd-menu-renderer button, .yt-lockup-metadata-view-model__menu-button button",
+                            )
+                        )
+                    ).click()
+                    time.sleep(2)
+                    # add to playlist click
+                    WebDriverWait(browser, 20).until(
+                        EC.element_to_be_clickable(
+                            (By.XPATH, '//span[contains(text(), "Save to playlist")]')
+                        )
+                    ).click()
                     time.sleep(1)
-                    try:
-                        check.click()
-                    except (ElementClickInterceptedException, ElementNotInteractableException) as error:
-                        pass
-                time.sleep(4)
+                    # identify correct playlist
+                    playlist_name_path = (
+                        '//*[contains(text(), "' + playlist_name + '")]'
+                    )
+                    # adds all elements that have playlist name text to list
+                    correct_playlist_check = browser.find_elements(
+                        By.XPATH, playlist_name_path
+                    )
+                    # try to click each item in list, if error, move to the next
+                    for check in correct_playlist_check:
+                        time.sleep(1)
+                        try:
+                            check.click()
+                        except (
+                            ElementClickInterceptedException,
+                            ElementNotInteractableException,
+                        ) as error:
+                            pass
+                    time.sleep(4)
 
-# quits
-time.sleep(3)
-browser.quit()
+
+def main():
+    upnext_folder = os.path.dirname(__file__)
+    settings_file = os.path.join(upnext_folder, "upnext_settings.yaml")
+    template_file = os.path.join(upnext_folder, "settings_template.yaml")
+
+    settings = load_yaml(settings_file, template_file)
+
+    required_keys = ["browser_choice", "playlist_link", "playlist_name", "subs"]
+
+    if any(settings.get(key) is None for key in required_keys):
+        print(
+            "Error: Missing required settings. Please check your upnext_settings.yaml file."
+        )
+        exit()
+    else:
+        browser_choice = settings["browser_choice"]
+        browser_profile = settings["browser_profile"]
+        edge_profile_name = settings["edge_profile_name"]
+        chromium_driver = settings["chromium_driver"]
+        playlist_link = settings["playlist_link"]
+        playlist_name = settings["playlist_name"]
+        subs_to_keep = settings["subs"]
+
+        browser = browser_setup(
+            browser=browser_choice,
+            profile_path=browser_profile,
+            edge_profile_name=edge_profile_name,
+            chromium_driver=chromium_driver,
+        )
+
+        try:
+            # access playlist
+            print("Accessing playlist...")
+            browser.get(playlist_link)
+            time.sleep(7)
+
+            print("Cleaning up playlist...")
+            playlist_cleanup(browser)
+            time.sleep(3)
+
+            print("Adding videos to playlist...")
+            browser.get("https://www.youtube.com/feed/subscriptions")
+            time.sleep(5)
+
+            playlist_build(browser, subs_to_keep, playlist_name)
+            time.sleep(3)
+        finally:
+            print("Closing browser...")
+            browser.quit()
+
+
+if __name__ == "__main__":
+    main()
